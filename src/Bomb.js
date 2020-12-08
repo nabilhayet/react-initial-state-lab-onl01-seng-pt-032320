81a19efcc216aa77e8f8d3a0ac4abc5f14ab6112
nabilhayet <nabil4457@yahoo.com>
2020-12-08 16:42:16 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,40 @@
 // your Bomb code here!
+
+import { Component } from "react";
+import React from "react"
+
+export default class Bomb extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = { 
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    // changeTimer = () => {
+    //     this.setState({
+    //       secondsLeft: this.props.secondsLeft-1
+    //     })
+    //   }
+
+    returnValue() {
+        if(this.state.secondsLeft !== 0){
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+           
+        }
+        else {
+            return 'Boom!'
+        }
+    
+    }
+
+render() {
+    return ( 
+        <div>
+            {this.returnValue()}
+        </div>
+    )
+}
+    
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
 // your ImageSlider code here!
+
+import { Component } from "react";
+import React from "react"
+
+export default class ImageSlider extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = { currentSlideIndex: 0}
+    }
+
+    render() {
+        return(`I am on slide ${this.state.currentSlideIndex}`)
+    }
+
+}
